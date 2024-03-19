2e196ee355070c96f13e66a950eb6ab767db77f9
PratyushRajGiri <160336390+PratyushRajGiri@users.noreply.github.com>
2024-03-19 09:04:09 +0545

    Somthing new

--- array/array 2 dimention/3.c
+++ array/array 2 dimention/3.c
@@ -1,41 +1,44 @@
-/*For displaying digonal of matrix */
+/* Difference of two given matrix */
 #include<stdio.h>
 #include<conio.h>
 int main()
 {
-   int  a[10][10],i,c;
-   printf("Enter the matrix (3*3)\n");
-   for(i=0;i<3;i++)
-{
-    for(c=0;c<3;c++)
+    int a[10][10],b[10][10],i,c;
+    printf("Enter  first 2*2 matrix\n");
+    for(i=0;i<2;i++)
     {
-        printf("[%d,%d]\t",i,c);
-        scanf("%d",&a[i][c]);
+        for(c=0;c<2;c++)
+        {
+            printf("matrix [%d][%d]=\t",i,c);
+            scanf("%d",&a[i][c]);
+        }
     }
-}
-     printf("Entered 3*3 matrix is\n");
-    for(i=0;i<3;i++)
+    printf("Enter second 2*2 matrix\n");
+    for(i=0;i<2;i++)
     {
-        for(c=0;c<3;c++)
+        for(c=0;c<2;c++)
+        {
+            printf("matrix [%d][%d]=\t",i,c);
+            scanf("%d",&b[i][c]);
+        }
+    }
+    for(i=0;i<2;i++)
+    {
+        for(c=0;c<2;c++)
+        {
+           a[i][c]=b[i][c]+a[i][c];
+        }
+    }
+    printf("sum of 2*2 matrix is\n");
+    for(i=0;i<2;i++)
+    {
+        for(c=0;c<2;c++)
         {
             printf("%d\t",a[i][c]);
         }
         printf("\n");
     }
 
- printf("the digonal of matrix (3*3) is\n");
-   for(i=0;i<3;i++)
-{
-    for(c=0;c<3;c++)
-    {
-        if(i==c)
-        {
-            printf("%d",a[i][c]);
-        }
-        
-    }
-    printf("\n");
-}
     return 0;
     getch();
 }
